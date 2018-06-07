dca2f1c3525c07e88042c1f19483a48df89bfcec
Stuart Lindstrom <stuartlindstrom@gmail.com>
2018-06-07 01:21:58 -0400

    stuff

--- server.js
+++ server.js
@@ -5,6 +5,41 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+/*const fs = require('fs');
+const portAudio = require('naudiodon');
+console.log(portAudio.getDevices());
+
+var ai = new portAudio.AudioInput({
+	channelCount: 2,
+	sampleFormat: portAudio.SampleFormat16Bit,
+	sampleRate: 44100,
+	deviceId: 4
+});
+ai.on('error', err => console.error);
+
+var ao = new portAudio.AudioOutput({
+	channelCount: 2,
+	sampleFormat: portAudio.SampleFormat16Bit,
+	sampleRate: 44100,
+	deviceId: 2
+});
+ao.on('error', err => console.error);
+
+//var rs = fs.createReadStream('./test.wav');
+//rs.on('end', () => ao.end());
+var ws = fs.createWriteStream('./data.raw');
+
+ai.pipe(ws);
+ai.start();
+//ao.start();
+
+process.on('SIGINT', () => {
+	console.log('quitting input');
+	ai.quit();
+});*/
+
+
+
 app.use(express.static(path.join(__dirname, 'src')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
